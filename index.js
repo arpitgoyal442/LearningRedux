5c09c77633b92b5b04fc76a296218b205f5018fa
arpitgoyal442 <arpitgoyal442@gmail.com>
2021-08-07 12:38:12 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 
 
 const redux=require("redux");
+const combineReducers=redux.combineReducers;
 
 //used to create redux-store
 const createStore=redux.createStore;
 
 
 //Setting the initial state(basically a js object) 
-const initialState={
+const cakeinitialState={
 
     noOfCakes:10
+    
 }
+
+const iceCreaminitialState={
+
+    
+    noOfIceCreams:20
+}
+
+
 
 //action-creator  returning a action(basically a js object having "type" property)
 function buyCake(){
@@ -22,8 +32,17 @@ function buyCake(){
     }
 }
 
+function buyIceCream(){
+
+    return{
+
+        type:"BUY_ICE_CREAM"
+        
+    }
+}
+
 //Making reducer --> take initial state and action and send new state to store
-const reducer= (state=initialState,action)=>{
+const cakereducer= (state=cakeinitialState,action)=>{
 
     switch(action.type){
 
@@ -33,6 +52,8 @@ const reducer= (state=initialState,action)=>{
             noOfCakes:state.noOfCakes-1
         }
 
+       
+
         default:
             return state
     }
@@ -40,7 +61,32 @@ const reducer= (state=initialState,action)=>{
 
 }
 
-const store= createStore(reducer);
+//Another Reducer To manage Just Icreams
+const iceCreamreducer= (state=iceCreaminitialState,action)=>{
+
+    switch(action.type){
+
+      
+        case "BUY_ICE_CREAM":
+            return{
+                ...state,
+                noOfIceCreams:state.noOfIceCreams-1
+            }
+
+
+        default:
+            return state
+    }
+
+
+}
+
+//It is Convention to say all the reducers combined as ROOT Reducer
+const rootReducer=combineReducers({
+    cake:cakereducer,
+    Ice:iceCreamreducer
+})
+const store= createStore(rootReducer);
 console.log(store.getState());
 
 //To listen for changes in store
@@ -48,6 +94,7 @@ store.subscribe(()=>console.log(store.getState()));
 
 //To change state in store
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
 
 
 
